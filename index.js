9a116d302b90a91cf46ccd974e36774cd21856ef
dlguun <mduk96@gmail.com>
2021-04-30 09:56:21 +0800

    done

--- index.js
+++ index.js
@@ -14,3 +14,11 @@ document.addEventListener('DOMContentLoaded', function() {
       toolbarEnabled: true
     });
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.dropdown-trigger');
+  var instances = M.Dropdown.init(elems, {
+    coverTrigger: false,
+    hover: true,
+  });
+});
