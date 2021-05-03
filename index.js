50dd0bb24c9ba833b250bbbaf20b4565818687a8
dlguun <mduk96@gmail.com>
2021-05-03 19:53:49 +0800

    pagination dutu

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.datepicker');
-    var instances = M.Datepicker.init(elems, {});
-});
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.timepicker');
-    var instances = M.Timepicker.init(elems, {});
+    var elems = document.querySelectorAll('.chips');
+    var instances = M.Chips.init(elems, {});
 });
