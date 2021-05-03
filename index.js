efbb0055c071b1f281408e492b197847d88a0416
dlguun <mduk96@gmail.com>
2021-05-03 18:22:51 +0800

    done

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-// var el = document.querySelector('.card-tabs');
-// var instance = M.Tabs.init(el, {
-
-// });
-// instance.select('tab_id');
+document.addEventListener('DOMContentLoaded', function() {
+    var el = document.querySelectorAll('.tabs');
+    var instance = M.Tabs.init(el, {
+        swipeable: false,
+    });    
+});
